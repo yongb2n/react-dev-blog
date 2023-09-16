8e5b98e1218939ba9bddb28ee9bd1758d8622091
binyongbin <kyb2475@gmail.com>
2023-09-16 11:49:49 +0900

    좋아요 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const post = '강남 우동 맛집';
+
+  const [title, setTitle] = useState(['남자 코트 추천', '인스타 맛집 추천', '예쁜 카페 추천'])
+  const [like, setLike] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="black-nav">
+          <div>BLOG^__^</div>
+        </div>
+
+        <div className="list">
+          <h4>{ title[0] } <span onClick={() => {setLike(like + 1)}}>🤍</span> { like } </h4>
+          <p>9월 16일 발행</p>
+        </div>
+        <div className="list">
+          <h4>{ title[1] }</h4>
+          <p>9월 16일 발행</p>
+        </div>
+        <div className="list">
+          <h4>{ title[2] }</h4>
+          <p>9월 16일 발행</p>
+        </div>
     </div>
   );
 }
